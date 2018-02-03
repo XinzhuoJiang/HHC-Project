229a13f785f03fabbb4c72e2c60e0c82927bb67e
XinzhuoJiang <xinzhuojiang@gmail.com>
2018-02-02 23:59:20 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import *
+import json
+import pandas as pd 
+from noshow import no_show_func
+from noshow import sav_load
+
+
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['GET'])
+def render_home_page():
+	return render_template('index.html')
+
+
+@app.route('/search/api', methods=['GET'])
+
+ 
+def get_info():
+
+
+    
+
+	#obj = request.get_json(force=True)
+	#print(obj)
+	#MRN = obj['MRN']
+	#year = obj['year']
+	#month = obj['month']
+	#day = obj['day']
+	#hour = obj['hour']
+	#minute = obj['minute']
+	#dept = obj['Dept']
+	#practice = obj['Practice']
+	#start_time = str(year)+"-"+str(month)+"-"+str(day)+ " " + str(hour) + ":" + str(minute)
+	#logit_file_path = 'S:/ExecutiveAdministration/Health Analytics/Internship Program/4. Appointment Show Rate/Logistic Regression/Data/Machine_Learning_Method_Data/L1_model.sav'
+	#pred_method_logit = sav_load(logit_file_path)
+	#logit_predict_result = no_show_func(MRN, dept, start_time, practice, pred_method_logit)
+	#jsonfiles = logit_predict_result.to_json(orient='index')
+	#print(jsonfiles)
+	return 'submit'      
+
+if __name__ == "__main__":
+	app.run()
